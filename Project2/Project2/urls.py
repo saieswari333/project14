1236ae2ba2442801c4bb56559822725f5a52586d
sandeepreddy50 <79621943+sandeepreddy50@users.noreply.github.com>
2023-05-28 16:46:27 +0530

    post form

--- Project2/Project2/urls.py
+++ Project2/Project2/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from  addapp import views
+from addapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('addapp',views.home),
+    path('addapp/', views.home),
+    path('addapp/add', views.add),
 ]
--- Project2/addapp/views.py
+++ Project2/addapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 def home(request):
     return render(request,'home.html')
-    def add(request):
-        
+def add(request):
+    x=request.POST["t1"]
+    y=request.POST["t2"]
+    i=int(x)
+    j=int(y)
+    z=i+j
+    res=HttpResponse("The sum is:"+str(z))
+    return res
+
+
+
 
